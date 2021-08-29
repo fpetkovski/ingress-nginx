d774aa0c4e167b4d30b8b1e81d02543818a1d426
Elvin Efendi <elvin.efendiyev@gmail.com>
2021-08-29 12:54:25 -0400

    delete unused artefacts from build.sh

--- images/nginx/rootfs/build.sh
+++ images/nginx/rootfs/build.sh
@@ -35,35 +35,8 @@ export NGINX_DIGEST_AUTH=1.0.0
 # Check for recent changes: https://github.com/yaoweibin/ngx_http_substitutions_filter_module/compare/v0.6.4...master
 export NGINX_SUBSTITUTIONS=b8a71eacc7f986ba091282ab8b1bbbc6ae1807e0
 
-# Check for recent changes: https://github.com/opentracing-contrib/nginx-opentracing/compare/v0.19.0...master
-# export NGINX_OPENTRACING_VERSION=0.19.0
-
-# Check for recent changes: https://github.com/opentracing/opentracing-cpp/compare/v1.6.0...master
-# export OPENTRACING_CPP_VERSION=f86b33f3d9e7322b1298ba62d5ffa7a9519c4c41
-
-# Check for recent changes: https://github.com/rnburn/zipkin-cpp-opentracing/compare/v0.5.2...master
-# export ZIPKIN_CPP_VERSION=f69593138ff84ca2f6bc115992e18ca3d35f344a
-
-# Check for recent changes: https://github.com/jbeder/yaml-cpp/compare/yaml-cpp-0.7.0...master
-# export YAML_CPP_VERSION=yaml-cpp-0.7.0
-
-# Check for recent changes: https://github.com/jaegertracing/jaeger-client-cpp/compare/v0.7.0...master
-# export JAEGER_VERSION=0.7.0
-
 # Check for recent changes: https://github.com/msgpack/msgpack-c/compare/cpp-3.3.0...master
 export MSGPACK_VERSION=3.3.0
-
-# Check for recent changes: https://github.com/DataDog/dd-opentracing-cpp/compare/v1.3.0...master
-# export DATADOG_CPP_VERSION=af53c523787cca108ae9f458ea5c962e48187a36
-
-# Check for recent changes: https://github.com/SpiderLabs/ModSecurity-nginx/compare/v1.0.2...master
-# export MODSECURITY_VERSION=1.0.2
-
-# Check for recent changes: https://github.com/SpiderLabs/ModSecurity/compare/v3.0.5...v3/master
-# export MODSECURITY_LIB_VERSION=v3.0.5
-
-# Check for recent changes: https://github.com/coreruleset/coreruleset/compare/v3.3.2...v3.3/master
-# export OWASP_MODSECURITY_CRS_VERSION=v3.3.2
 
 # Check for recent changes: https://github.com/openresty/lua-nginx-module/compare/v0.10.20...master
 export LUA_NGX_VERSION=b721656a9127255003b696b42ccc871c7ec18d59
@@ -76,8 +49,6 @@ export LUA_UPSTREAM_VERSION=8aa93ead98ba2060d4efd594ae33a35d153589bf
 
 # Check for recent changes: https://github.com/openresty/lua-cjson/compare/2.1.0.8...openresty:master
 export LUA_CJSON_VERSION=4b350c531de3d71008c77ae94e59275b8371b4dc
-
-# export NGINX_INFLUXDB_VERSION=5b09391cb7b9a889687c0aa67964c06a2d933e8b
 
 # Check for recent changes: https://github.com/leev/ngx_http_geoip2_module/compare/3.3...master
 export GEOIP2_VERSION=a26c6beed77e81553686852dceb6c7fdacc5970d
@@ -215,24 +186,6 @@ get_src f09851e6309560a8ff3e901548405066c83f1f6ff88aa7171e0763bd9514762b \
 get_src a98b48947359166326d58700ccdc27256d2648218072da138ab6b47de47fbd8f \
         "https://github.com/yaoweibin/ngx_http_substitutions_filter_module/archive/$NGINX_SUBSTITUTIONS.tar.gz"
 
-# get_src 6f97776ebdf019b105a755c7736b70bdbd7e575c7f0d39db5fe127873c7abf17 \
-#         "https://github.com/opentracing-contrib/nginx-opentracing/archive/v$NGINX_OPENTRACING_VERSION.tar.gz"
-
-# get_src cbe625cba85291712253db5bc3870d60c709acfad9a8af5a302673d3d201e3ea \
-#         "https://github.com/opentracing/opentracing-cpp/archive/$OPENTRACING_CPP_VERSION.tar.gz"
-
-# get_src 71de3d0658935db7ccea20e006b35e58ddc7e4c18878b9523f2addc2371e9270 \
-#         "https://github.com/rnburn/zipkin-cpp-opentracing/archive/$ZIPKIN_CPP_VERSION.tar.gz"
-
-# get_src f8d3ff15520df736c5e20e91d5852ec27e0874566c2afce7dcb979e2298d6980 \
-#         "https://github.com/SpiderLabs/ModSecurity-nginx/archive/v$MODSECURITY_VERSION.tar.gz"
-
-# get_src 43e6a9fcb146ad871515f0d0873947e5d497a1c9c60c58cb102a97b47208b7c3 \
-#         "https://github.com/jbeder/yaml-cpp/archive/$YAML_CPP_VERSION.tar.gz"
-
-# get_src 3a3a03060bf5e3fef52c9a2de02e6035cb557f389453d8f3b0c1d3d570636994 \
-#         "https://github.com/jaegertracing/jaeger-client-cpp/archive/v$JAEGER_VERSION.tar.gz"
-
 get_src 754c3ace499a63e45b77ef4bcab4ee602c2c414f58403bce826b76ffc2f77d0b \
         "https://github.com/msgpack/msgpack-c/archive/cpp-$MSGPACK_VERSION.tar.gz"
 
@@ -260,12 +213,6 @@ else
 get_src 1ee6dad809a5bb22efb45e6dac767f7ce544ad652d353a93d7f26b605f69fe3f \
         "https://github.com/openresty/luajit2/archive/v$LUAJIT_VERSION.tar.gz"
 fi
-
-# get_src f29393f2cd9288105a0029a6a324fe1f7558a9e7e852d59a6355f7581bb90e30 \
-#         "https://github.com/DataDog/dd-opentracing-cpp/archive/$DATADOG_CPP_VERSION.tar.gz"
-
-# get_src 1af5a5632dc8b00ae103d51b7bf225de3a7f0df82f5c6a401996c080106e600e \
-#         "https://github.com/influxdata/nginx-influxdb-module/archive/$NGINX_INFLUXDB_VERSION.tar.gz"
 
 get_src 4c1933434572226942c65b2f2b26c8a536ab76aa771a3c7f6c2629faa764976b \
         "https://github.com/leev/ngx_http_geoip2_module/archive/$GEOIP2_VERSION.tar.gz"
@@ -320,14 +267,6 @@ get_src b8dbd502751140993a852381bcd8e98a402454596bd91838c1e51268d42db261 \
 get_src 0fb790e394510e73fdba1492e576aaec0b8ee9ef08e3e821ce253a07719cf7ea \
         "https://github.com/ElvinEfendi/lua-resty-global-throttle/archive/v$LUA_RESTY_GLOBAL_THROTTLE_VERSION.tar.gz"
 
-# # improve compilation times
-# CORES=$(($(grep -c ^processor /proc/cpuinfo) - 1))
-
-# export MAKEFLAGS=-j${CORES}
-# export CTEST_BUILD_FLAGS=${MAKEFLAGS}
-# export HUNTER_JOBS_NUMBER=${CORES}
-# export HUNTER_USE_CACHE_SERVERS=true
-
 # Install luajit from openresty fork
 export LUAJIT_LIB=/usr/local/lib
 export LUA_LIB_DIR="$LUAJIT_LIB/lua"
@@ -345,101 +284,6 @@ cd "$BUILD_PATH"
 # Git tuning
 git config --global --add core.compression -1
 
-# # build opentracing lib
-# cd "$BUILD_PATH/opentracing-cpp-$OPENTRACING_CPP_VERSION"
-# mkdir .build
-# cd .build
-
-# cmake -DCMAKE_BUILD_TYPE=Release \
-#       -DBUILD_TESTING=OFF \
-#       -DBUILD_SHARED_LIBS=OFF \
-#       -DBUILD_MOCKTRACER=OFF \
-#       -DBUILD_STATIC_LIBS=ON \
-#       -DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=true \
-#       ..
-
-# make
-# make install
-
-# # build yaml-cpp
-# # TODO @timmysilv: remove this and jaeger sed calls once it is fixed in jaeger-client-cpp
-# cd "$BUILD_PATH/yaml-cpp-$YAML_CPP_VERSION"
-# mkdir .build
-# cd .build
-
-# cmake -DCMAKE_BUILD_TYPE=Release \
-#       -DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=true \
-#       -DYAML_BUILD_SHARED_LIBS=ON \
-#       -DYAML_CPP_BUILD_TESTS=OFF \
-#       -DYAML_CPP_BUILD_TOOLS=OFF \
-#       ..
-
-# make
-# make install
-
-# # build jaeger lib
-# cd "$BUILD_PATH/jaeger-client-cpp-$JAEGER_VERSION"
-# sed -i 's/-Werror/-Wno-psabi/' CMakeLists.txt
-# # use the above built yaml-cpp instead until a new version of jaeger-client-cpp fixes the yaml-cpp issue
-# # tl;dr new hunter is needed for new yaml-cpp, but new hunter has a conflict with old Thrift and new Boost
-# sed -i 's/hunter_add_package(yaml-cpp)/#hunter_add_package(yaml-cpp)/' CMakeLists.txt
-# sed -i 's/yaml-cpp::yaml-cpp/yaml-cpp/' CMakeLists.txt
-
-# cat <<EOF > export.map
-# {
-#     global:
-#         OpenTracingMakeTracerFactory;
-#     local: *;
-# };
-# EOF
-
-# mkdir .build
-# cd .build
-
-# cmake -DCMAKE_BUILD_TYPE=Release \
-#       -DBUILD_TESTING=OFF \
-#       -DJAEGERTRACING_BUILD_EXAMPLES=OFF \
-#       -DJAEGERTRACING_BUILD_CROSSDOCK=OFF \
-#       -DJAEGERTRACING_COVERAGE=OFF \
-#       -DJAEGERTRACING_PLUGIN=ON \
-#       -DHUNTER_CONFIGURATION_TYPES=Release \
-#       -DBUILD_SHARED_LIBS=OFF \
-#       -DJAEGERTRACING_WITH_YAML_CPP=ON \
-#       -DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=true \
-#       ..
-
-# make
-# make install
-
-# export HUNTER_INSTALL_DIR=$(cat _3rdParty/Hunter/install-root-dir) \
-
-# mv libjaegertracing_plugin.so /usr/local/lib/libjaegertracing_plugin.so
-
-
-# # build zipkin lib
-# cd "$BUILD_PATH/zipkin-cpp-opentracing-$ZIPKIN_CPP_VERSION"
-
-# cat <<EOF > export.map
-# {
-#     global:
-#         OpenTracingMakeTracerFactory;
-#     local: *;
-# };
-# EOF
-
-# mkdir .build
-# cd .build
-
-# cmake -DCMAKE_BUILD_TYPE=Release \
-#       -DBUILD_SHARED_LIBS=OFF \
-#       -DBUILD_PLUGIN=ON \
-#       -DBUILD_TESTING=OFF \
-#       -DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=true \
-#       ..
-
-# make
-# make install
-
 # build msgpack lib
 cd "$BUILD_PATH/msgpack-c-cpp-$MSGPACK_VERSION"
 
@@ -453,20 +297,6 @@ cmake -DCMAKE_BUILD_TYPE=Release \
 
 make
 make install
-
-# # build datadog lib
-# cd "$BUILD_PATH/dd-opentracing-cpp-$DATADOG_CPP_VERSION"
-
-# mkdir .build
-# cd .build
-
-# cmake -DCMAKE_BUILD_TYPE=Release \
-#       -DBUILD_TESTING=OFF \
-#       -DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=true \
-#       ..
-
-# make
-# make install
 
 # Get Brotli source and deps
 cd "$BUILD_PATH"
@@ -484,83 +314,6 @@ cd ssdeep/
 
 make
 make install
-
-# # build modsecurity library
-# cd "$BUILD_PATH"
-# git clone --depth=1 -b $MODSECURITY_LIB_VERSION https://github.com/SpiderLabs/ModSecurity
-# cd ModSecurity/
-# git submodule init
-# git submodule update
-
-# sh build.sh
-
-# # https://github.com/SpiderLabs/ModSecurity/issues/1909#issuecomment-465926762
-# sed -i '115i LUA_CFLAGS="${LUA_CFLAGS} -DWITH_LUA_JIT_2_1"' build/lua.m4
-# sed -i '117i AC_SUBST(LUA_CFLAGS)' build/lua.m4
-
-# ./configure \
-#   --disable-doxygen-doc \
-#   --disable-doxygen-html \
-#   --disable-examples
-
-# make
-# make install
-
-# mkdir -p /etc/nginx/modsecurity
-# cp modsecurity.conf-recommended /etc/nginx/modsecurity/modsecurity.conf
-# cp unicode.mapping /etc/nginx/modsecurity/unicode.mapping
-
-# # Replace serial logging with concurrent
-# sed -i 's|SecAuditLogType Serial|SecAuditLogType Concurrent|g' /etc/nginx/modsecurity/modsecurity.conf
-
-# # Concurrent logging implies the log is stored in several files
-# echo "SecAuditLogStorageDir /var/log/audit/" >> /etc/nginx/modsecurity/modsecurity.conf
-
-# # Download owasp modsecurity crs
-# cd /etc/nginx/
-
-# git clone -b $OWASP_MODSECURITY_CRS_VERSION https://github.com/coreruleset/coreruleset
-# mv coreruleset owasp-modsecurity-crs
-# cd owasp-modsecurity-crs
-
-# mv crs-setup.conf.example crs-setup.conf
-# mv rules/REQUEST-900-EXCLUSION-RULES-BEFORE-CRS.conf.example rules/REQUEST-900-EXCLUSION-RULES-BEFORE-CRS.conf
-# mv rules/RESPONSE-999-EXCLUSION-RULES-AFTER-CRS.conf.example rules/RESPONSE-999-EXCLUSION-RULES-AFTER-CRS.conf
-# cd ..
-
-# # OWASP CRS v3 rules
-# echo "
-# Include /etc/nginx/owasp-modsecurity-crs/crs-setup.conf
-# Include /etc/nginx/owasp-modsecurity-crs/rules/REQUEST-900-EXCLUSION-RULES-BEFORE-CRS.conf
-# Include /etc/nginx/owasp-modsecurity-crs/rules/REQUEST-901-INITIALIZATION.conf
-# Include /etc/nginx/owasp-modsecurity-crs/rules/REQUEST-903.9001-DRUPAL-EXCLUSION-RULES.conf
-# Include /etc/nginx/owasp-modsecurity-crs/rules/REQUEST-903.9002-WORDPRESS-EXCLUSION-RULES.conf
-# Include /etc/nginx/owasp-modsecurity-crs/rules/REQUEST-905-COMMON-EXCEPTIONS.conf
-# Include /etc/nginx/owasp-modsecurity-crs/rules/REQUEST-910-IP-REPUTATION.conf
-# Include /etc/nginx/owasp-modsecurity-crs/rules/REQUEST-911-METHOD-ENFORCEMENT.conf
-# Include /etc/nginx/owasp-modsecurity-crs/rules/REQUEST-912-DOS-PROTECTION.conf
-# Include /etc/nginx/owasp-modsecurity-crs/rules/REQUEST-913-SCANNER-DETECTION.conf
-# Include /etc/nginx/owasp-modsecurity-crs/rules/REQUEST-920-PROTOCOL-ENFORCEMENT.conf
-# Include /etc/nginx/owasp-modsecurity-crs/rules/REQUEST-921-PROTOCOL-ATTACK.conf
-# Include /etc/nginx/owasp-modsecurity-crs/rules/REQUEST-930-APPLICATION-ATTACK-LFI.conf
-# Include /etc/nginx/owasp-modsecurity-crs/rules/REQUEST-931-APPLICATION-ATTACK-RFI.conf
-# Include /etc/nginx/owasp-modsecurity-crs/rules/REQUEST-932-APPLICATION-ATTACK-RCE.conf
-# Include /etc/nginx/owasp-modsecurity-crs/rules/REQUEST-933-APPLICATION-ATTACK-PHP.conf
-# Include /etc/nginx/owasp-modsecurity-crs/rules/REQUEST-934-APPLICATION-ATTACK-NODEJS.conf
-# Include /etc/nginx/owasp-modsecurity-crs/rules/REQUEST-941-APPLICATION-ATTACK-XSS.conf
-# Include /etc/nginx/owasp-modsecurity-crs/rules/REQUEST-942-APPLICATION-ATTACK-SQLI.conf
-# Include /etc/nginx/owasp-modsecurity-crs/rules/REQUEST-943-APPLICATION-ATTACK-SESSION-FIXATION.conf
-# Include /etc/nginx/owasp-modsecurity-crs/rules/REQUEST-944-APPLICATION-ATTACK-JAVA.conf
-# Include /etc/nginx/owasp-modsecurity-crs/rules/REQUEST-949-BLOCKING-EVALUATION.conf
-# Include /etc/nginx/owasp-modsecurity-crs/rules/RESPONSE-950-DATA-LEAKAGES.conf
-# Include /etc/nginx/owasp-modsecurity-crs/rules/RESPONSE-951-DATA-LEAKAGES-SQL.conf
-# Include /etc/nginx/owasp-modsecurity-crs/rules/RESPONSE-952-DATA-LEAKAGES-JAVA.conf
-# Include /etc/nginx/owasp-modsecurity-crs/rules/RESPONSE-953-DATA-LEAKAGES-PHP.conf
-# Include /etc/nginx/owasp-modsecurity-crs/rules/RESPONSE-954-DATA-LEAKAGES-IIS.conf
-# Include /etc/nginx/owasp-modsecurity-crs/rules/RESPONSE-959-BLOCKING-EVALUATION.conf
-# Include /etc/nginx/owasp-modsecurity-crs/rules/RESPONSE-980-CORRELATION.conf
-# Include /etc/nginx/owasp-modsecurity-crs/rules/RESPONSE-999-EXCLUSION-RULES-AFTER-CRS.conf
-# " > /etc/nginx/owasp-modsecurity-crs/nginx-modsecurity.conf
 
 # build nginx
 cd "$BUILD_PATH/nginx-$NGINX_VERSION"
@@ -629,9 +382,6 @@ WITH_MODULES=" \
   --add-module=$BUILD_PATH/lua-upstream-nginx-module-$LUA_UPSTREAM_VERSION \
   --add-module=$BUILD_PATH/nginx_ajp_module-${NGINX_AJP_VERSION} \
   --add-dynamic-module=$BUILD_PATH/nginx-http-auth-digest-$NGINX_DIGEST_AUTH \
-#   --add-dynamic-module=$BUILD_PATH/nginx-influxdb-module-$NGINX_INFLUXDB_VERSION \
-#   --add-dynamic-module=$BUILD_PATH/nginx-opentracing-$NGINX_OPENTRACING_VERSION/opentracing \
-#   --add-dynamic-module=$BUILD_PATH/ModSecurity-nginx-$MODSECURITY_VERSION \
   --add-dynamic-module=$BUILD_PATH/ngx_http_geoip2_module-${GEOIP2_VERSION} \
   --add-dynamic-module=$BUILD_PATH/ngx_brotli"
 
@@ -737,16 +487,12 @@ writeDirs=( \
   /var/log/nginx \
 );
 
-# addgroup -Sg 101 www-data
 adduser -S -D -H -u 101 -h /usr/local/nginx -s /sbin/nologin -G www-data -g www-data www-data
 
 for dir in "${writeDirs[@]}"; do
   mkdir -p ${dir};
   chown -R www-data.www-data ${dir};
 done
-
-# rm -rf /etc/nginx/owasp-modsecurity-crs/.git
-# rm -rf /etc/nginx/owasp-modsecurity-crs/util/regression-tests
 
 # remove .a files
 find /usr/local -name "*.a" -print | xargs /bin/rm
