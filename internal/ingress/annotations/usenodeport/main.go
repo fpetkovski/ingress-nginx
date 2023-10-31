ea390cb2f04234d6bd2c74e0077d0c5fa8f15c82
Elvin Efendiev <elvin.efendiev@shopify.com>
2023-10-31 12:53:32 -0400

    introduce use-node-port annotation
    Co-authored-by: Ian Ker-Seymer <ian.kerseymer@shopify.com>

---
+++ internal/ingress/annotations/usenodeport/main.go
@@ -0,0 +1,43 @@
+/*
+Copyright 2023 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package usenodeport
+
+import (
+	networking "k8s.io/api/networking/v1"
+
+	"k8s.io/ingress-nginx/internal/ingress/annotations/parser"
+	"k8s.io/ingress-nginx/internal/ingress/errors"
+	"k8s.io/ingress-nginx/internal/ingress/resolver"
+)
+
+type useNodePort struct {
+	r resolver.Resolver
+}
+
+// NewParser creates a new useNodePort annotation parser
+func NewParser(r resolver.Resolver) parser.IngressAnnotation {
+	return useNodePort{r}
+}
+
+func (useNodePort) Parse(ing *networking.Ingress) (interface{}, error) {
+	val, err := parser.GetBoolAnnotation("use-node-port", ing)
+	if err == errors.ErrMissingAnnotations {
+		return false, nil
+	}
+
+	return val, nil
+}
